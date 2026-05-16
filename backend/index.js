6beaec07fc3332c1d2a32d825733232032944b3c
Gopa Dutta <gopadutta04@gmail.com>
2026-05-16 08:16:31 +0530

    uri secrecy done

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const MongoClient = mongodb.MongoClient
 const mongo_username = process.env.MONGO_USER
 const mongo_password = process.env.MONGO_PASS
 
-const uri = `mongodb://gopadutta04_db_user:disha11@ac-mbphlfx-shard-00-00.e5fzk2b.mongodb.net:27017,ac-mbphlfx-shard-00-01.e5fzk2b.mongodb.net:27017,ac-mbphlfx-shard-00-02.e5fzk2b.mongodb.net:27017/?ssl=true&replicaSet=atlas-ezaj2s-shard-0&authSource=admin&appName=Cluster0`
+const uri = process.env.MONGO_URI
 
 
 const port = 8000  
